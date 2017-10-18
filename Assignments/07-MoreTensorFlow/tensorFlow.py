09847fed1f7eb6f9238cd54a35a45a4e17cd6b9a
nikhilvangeti <vangetinikhil93@yahoo.in>
2017-10-18 12:26:09 -0500

    Update tensorFlow.py

--- Assignments/07-MoreTensorFlow/tensorFlow.py
+++ Assignments/07-MoreTensorFlow/tensorFlow.py
@@ -1,1 +1,193 @@
-Hello
+# Import `tensorflow`
+import tensorflow as tf
+import os
+import skimage
+from skimage import data
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+from skimage.color import rgb2gray
+from skimage import transform 
+
+# Initialize two constants
+x1 = tf.constant([1,2,3,4])
+x2 = tf.constant([5,6,7,8])
+
+# Multiply
+result = tf.multiply(x1, x2)
+
+# Initialize Session and run `result`
+with tf.Session() as sess:
+  output = sess.run(result)
+  print(output)
+  
+def load_data(data_directory):
+    directories = [d for d in os.listdir(data_directory) 
+                   if os.path.isdir(os.path.join(data_directory, d))]
+    labels = []
+    images = []
+    for d in directories:
+        label_directory = os.path.join(data_directory, d)
+        file_names = [os.path.join(label_directory, f) 
+                      for f in os.listdir(label_directory) 
+                      if f.endswith(".ppm")]
+        for f in file_names:
+            images.append(skimage.data.imread(f))
+            labels.append(int(d))
+    return images, labels
+
+ROOT_PATH = "/Users/nikhil"
+train_data_directory = os.path.join(ROOT_PATH, "Training")
+test_data_directory = os.path.join(ROOT_PATH, "Testing")
+
+images, labels = load_data(train_data_directory)
+
+images=np.array(images)
+labels=np.array(labels)
+
+print(images.ndim)
+
+# Print the number of `images`'s elements
+print(images.size)
+
+
+# Print the `labels` dimensions
+print(labels.ndim)
+
+# Print the number of `labels`'s elements
+print(labels.size)
+
+# Count the number of labels
+print(len(set(labels)))
+
+# Make a histogram with 62 bins of the `labels` data
+plt.hist(labels, 62)
+
+# Show the plot
+plt.show()
+
+traffic_signs = [300, 2250, 3650, 4000]
+
+for i in range(len(traffic_signs)):
+    plt.subplot(1, 4, i+1)
+    plt.axis('off')
+    plt.imshow(images[traffic_signs[i]])
+    plt.subplots_adjust(wspace=0.5)
+    plt.show()
+    print("shape: {0}, min: {1}, max: {2}".format(images[traffic_signs[i]].shape, 
+                                                  images[traffic_signs[i]].min(), 
+                                                  images[traffic_signs[i]].max()))
+plt.show()
+
+
+unique_labels = set(labels)
+
+# Initialize the figure
+plt.figure(figsize=(15, 15))
+
+# Set a counter
+i = 1
+
+# For each unique label,
+#for label in unique_labels:
+ #   # You pick the first image for each label
+  #  image = images[labels.tolist().index(label)]
+   # # Define 64 subplots 
+    #plt.subplot(8, 8, i)
+    # Don't include axes
+    #plt.axis('off')
+    # Add a title to each subplot 
+    #plt.title("Label {0} ({1})".format(label, labels.tolist().count(label)))
+    # Add 1 to the counter
+    #i += 1
+    # And you plot this first image 
+    #Splt.imshow(image)
+  
+images28 = [transform.resize(image, (28, 28)) for image in images]
+
+images28=np.array(images28)
+
+# Convert `images28` to grayscale
+images28 = rgb2gray(images28)
+
+traffic_signs = [300, 2250, 3650, 4000]
+
+for i in range(len(traffic_signs)):
+    plt.subplot(1, 4, i+1)
+    plt.axis('off')
+    plt.imshow(images28[traffic_signs[i]], cmap="gray")
+    plt.subplots_adjust(wspace=0.5)
+    
+# Show the plot
+plt.show()
+
+#sparse_softmax_cross_entropy_with_logits()
+
+# Initialize placeholders 
+x = tf.placeholder(dtype = tf.float32, shape = [None, 28, 28])
+y = tf.placeholder(dtype = tf.int32, shape = [None])
+
+# Flatten the input data
+images_flat = tf.contrib.layers.flatten(x)
+
+# Fully connected layer 
+logits = tf.contrib.layers.fully_connected(images_flat, 62, tf.nn.relu)
+
+# Define a loss function
+loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, 
+                                                                    logits = logits))
+# Define an optimizer 
+train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+
+# Convert logits to label indexes
+correct_pred = tf.argmax(logits, 1)
+
+# Define an accuracy metric
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+print("images_flat: ", images_flat)
+print("logits: ", logits)
+print("loss: ", loss)
+print("predicted_labels: ", correct_pred)
+
+tf.set_random_seed(1234)
+sess = tf.Session()
+
+sess.run(tf.global_variables_initializer())
+
+for i in range(201):
+        print('EPOCH', i)
+        _, accuracy_val = sess.run([train_op, accuracy], feed_dict={x: images28, y: labels})
+        if i % 10 == 0:
+            print("Loss: ", loss)
+        print('DONE WITH EPOCH')
+        
+test_images, test_labels = load_data(test_data_directory)
+
+test_images=np.array(test_images)
+test_labels=np.array(test_labels)
+
+# Transform the images to 28 by 28 pixels
+test_images28 = [transform.resize(image, (28, 28)) for image in test_images]
+
+# Convert to grayscale
+test_images28 = rgb2gray(np.array(test_images28))
+
+# Run predictions against the full test set.
+predicted = sess.run([correct_pred], feed_dict={x: test_images28})[0]
+
+# Calculate correct matches 
+match_count = sum([int(y1 == y1_) for y1, y1_ in zip(test_labels, predicted)])
+
+# Calculate the accuracy
+accuracy = match_count / len(test_labels)
+
+#print match_count
+print(match_count)
+
+#print test_labels
+print(len(test_labels))
+
+# Print the accuracy
+print("Accuracy: {:.3f}".format(accuracy))
+    
